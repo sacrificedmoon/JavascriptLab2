e475b5794f0a336c1b65cc29b0a807c6965a2761
SofiaLindgren <sofia.lindgren@iths.se>
2020-03-23 23:42:20 +0100

    Added functions to get API-key, add new books and see stored books, however, nothing except getting new API-keys work

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + apiKey;
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=';
 var apiKey;
 const maxAttempts = 10;
 let numOfAttempts = 0;
+let bookList= [];
 
 function getAPIKey () {
     let APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
@@ -10,3 +11,44 @@ function getAPIKey () {
         .then((response) => response.json())
         .then((jsonResponse) => {apiKeyResult.innerHTML = jsonResponse.key; apiKey = jsonResponse.key});
 };
+
+function addBook () {
+    const addBooksQuery = '&op=insert';
+    let bookTitle = document.getElementById('titleForm').value;
+    let authorName = document.getElementById('authorForm').value;
+    const endpoint = baseUrl + apiKey + addBooksQuery + '&title=' + bookTitle + '&author=' + authorName;
+    fetch(endpoint)
+    .then((response) => response.json())
+    .then(json => {
+        if (json.status === 'success') {
+            document.getElementById('bookMessage').innerHTML = 'Succesfully added a book';
+            viewBooks();
+        } else {
+            document.getElementById('bookMessage').innerHTML = 'Failed to add a book';
+        }
+    })
+};
+
+function viewBooks () {
+    const viewBooksQuery = '&op=select';
+    const endpoint = baseUrl + apiKey + viewBooksQuery;
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(json => {
+        if(json.status === 'success') {            
+            bookList = jsonResponse['data'];
+            let output = '';
+            bookList.forEach(function (element) {
+                output += '<ul>' +
+                    '<li> ID: ' + element.id + '</li>' +
+                    '<li> Title: ' + element.title + '</li>' +
+                    '<li> Author: ' + element.author + '</li>' +
+                    '</ul>'; });
+            document.getElementById('showBooksDiv').innerHTML = output;		
+        }
+    });
+}
+
+document.getElementById("viewBooksBtn").addEventListener("click", viewBooks());
+document.getElementById("addBookBtn").addEventListener("click", addBook());
+document.getElementById("getAPIKey").addEventListener("click", getAPIKey());
