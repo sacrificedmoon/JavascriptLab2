0d8156e4ac362a4b4595efde6d8679a55e441b73
SofiaLindgren <sofia.lindgren@iths.se>
2020-03-23 19:46:29 +0100

    Added function to fetch an APIKey

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=';
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + apiKey;
 var apiKey;
 const maxAttempts = 10;
 let numOfAttempts = 0;
+
+function getAPIKey () {
+    let APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+    let apiKeyResult = document.getElementById("keyResult");
+    fetch(APIUrl)
+        .then((response) => response.json())
+        .then((jsonResponse) => {apiKeyResult.innerHTML = jsonResponse.key; apiKey = jsonResponse.key});
+};
