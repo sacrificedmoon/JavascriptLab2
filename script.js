34ccdb2782db737b6be5e456e9302dd158ec0699
SofiaLindgren <sofialindgren.93@hotmail.se>
2020-05-31 13:09:02 +0200

    Final changes

--- script.js
+++ script.js
@@ -1,85 +1,88 @@
-let apiKey = localStorage.getItem('accessKey');
-let baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + apiKey;
+let APIKey;
+let baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + APIKey;
+let apiKeyLabel = document.getElementById('keyResult');
+let savedBooks;
+let bookMessage;
+let authorName;
+let bookTitle;
+let newAuthorName;
+let newBookTitle;
+let bookList;
+let viewRequest = baseUrl + '&op=select';
+let addRequest = baseUrl + '&op=insert' + '&title=' + newBookTitle + '&author=' + newAuthorName;
+let APIUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?requestKey';
 let maxAttempts = 10;
 let numOfAttempts = 0;
-let bookList;
-let savedBooks;
-let bookMessage; 
-let bookTitle;
-let authorName;
-let fetchBooks = baseUrl + '&op=select';
-let APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
-let addBooksQuery = baseUrl + '&op=insert' + '&title=' + bookTitle + '&author=' + authorName;
 
 window.addEventListener('load', () => {
-    savedBooks = document.getElementById('savedBooks');
-    bookMessage = document.getElementById('statusReport')
-    bookTitle = document.getElementById('titleForm').value;
-    authorName = document.getElementById('authorForm').value;
-})
+    savedBooks = document.getElementById('savedBookList');
+    apiKeyLabel = document.getElementById('keyResult');
+    authorName = document.getElementById('authorForm');
+    bookTitle = document.getElementById('titleForm');
+    bookMessage = document.getElementById('statusReport');
+    });
 
 function getAPIKey () {
     fetch(APIUrl)
-        .then((response) => response.json())
+        .then(response => {
+            return response.json()
+        })
         .then((json) => {
-            if (json.status === "success") {
-                apiKey = json.key;
-                document.getElementById('keyResult').innerHTML = ('Your key is ' + apiKey);
-                savedBooks.innerHTML = '';
+            if (json.status === 'success') {
+                APIKey = json.key;
+                baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + APIKey;
+                viewRequest = baseUrl + '&op=select';
+                apiKeyLabel.innerHTML = `Your API-Key is: ${APIKey}`;
+                savedBookList.innerHTML = '';
             }
-            
-        });
+        })
 }
 
 function viewBooks () {
-    if (savedBooks !== null) {
-        savedBooks.innerHTML = '';
-    }
-    fetch(fetchBooks)
-    .then(response => 
-        response.json())
-    .then(json => {
-        if (json.status === 'success') {
-            bookList = [];
-            json.data.forEach(element => {
-                bookList.push({
-                    title: element.title, author: element.author, id: element.id
+    fetch(viewRequest)
+        .then(response => 
+            response.json())
+        .then(json => {
+            if(json.status === 'success') {
+                bookList = [];
+                json.data.forEach(element => {
+                    bookList.push({
+                        title: element.title, author: element.author, id: element.id
+                    });
+                    savedBookList.innerHTML = '';
+                    for(let i=0; i<bookList.length; i++) {
+                        let id = bookList[i].id;
+                        let author = bookList[i].author;
+                        let title = bookList[i].title;
+                        let createNewLi = document.createElement('li');
+                        createNewLi.innerHTML = ' | ' + id + ' | ' + author + ' | ' + title + ' | ';
+                        savedBookList.appendChild(createNewLi);
+                    }
                 });
-                savedBooks.innerHTML = '';
-                for(let i=0; i < bookList.lenght; i++) {
-                  let id = bookList[i].id;
-                  let author = bookList[i].author;
-                  let title = bookList[i].title;
-                  let createLi = document.createElement('li');
-                  createLi.innerHTML = ' | ' + id + ' | ' + author + ' | ' + title + ' | ';
-                  savedBooks.appendChild(createLi);
-                }
-            });
-        } else {
-           console.log('Failed');
-           return viewBooks(numOfAttempts++);
-        } 
-          
-    })
+            } else {
+                return viewBooks(numOfAttempts ++);
+            }
+            numOfAttempts = 0;
+        })
 }
 
 function addBook () {
-    numOfAttempts++;
-    if (numOfAttempts >= maxAttempts) {
-        bookMessage.innerHTML = 'Failed to add book';
-    } 
-    fetch(addBooksQuery)
-    .then((response) => response.json())
-    .then((json) => {
-        if (json.status === 'success') {
-            bookMessage.innerHTML = (`Book added.`);
-            console.log('Operation was a success');
-            document.getElementById('titleForm').value = '';
-            document.getElementById('authorForm').value = '';
-            numOfAttempts = 0;
+    if(maxAttempts >= 10) {
+        bookMessage.innerHTML = 'Failed to add the book.';
+    }
+    const newBookTitle = '&title=' + bookTitle.value;
+    const newAuthorName = '&author=' + authorName.value;
+    let request = baseUrl + '&op=insert' + newBookTitle + newAuthorName;
+    fetch(request)
+    .then(response => response.json())
+    .then(json => {
+        if(json.status === 'success') {
+            bookMessage.innerHTML = `Succesfully added book!`;
+            bookTitle.value = '';
+            authorName.value = '';
+            maxAttempts = 0;
         } else {
-           return addBook(); 
+            return addBook(maxAttempts++);
         }
-    });
+    })
 }
-
